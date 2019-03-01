b7a17d5cef425e26c9aa0b95796bea669414f479
awesome-edition-6075 <zachpardee@gmail.com>
2019-03-01 09:28:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,11 @@ def countdown(number)
   end
   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  while number != 0 do
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
